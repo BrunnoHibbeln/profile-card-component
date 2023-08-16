fbf9cf2d7cc8860e07224bb98a558afdc9667f7c
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-16 13:26:45 -0300

    modifying styles

--- app/page.js
+++ app/page.js
@@ -9,23 +9,66 @@ export default function Home() {
   return (
     <main className="relative bg-dark-cyan h-screen overflow-hidden">
       <Image
-        className="absolute w-[50rem] h-[50rem] -left-32 -top-[28rem]"
+        className="absolute w-[80rem] h-[80rem] -left-52 -top-[50rem]"
         src={bgTop}
         alt="Circle top"
       />
       <Image
-        className="absolute w-[50rem] h-[50rem] -right-32 -bottom-[28rem]"
+        className="absolute w-[80rem] h-[80rem] -right-52 -bottom-[50rem]"
         src={bgBottom}
         alt="Circle bottom"
       />
       <section className="absolute w-[100%] h-screen bg-transparent flex justify-center items-center">
-        <section className="relative w-[20rem] h-[21rem] bg-white rounded-xl">
-          <Image className="rounded-t-xl" src={bgCard} alt="Background Card" />
+        <section className="relative w-[30rem] h-[31rem] bg-white rounded-3xl">
           <Image
-            className="absolute right-[36%] top-[25%] rounded-full flex justify-center"
-            src={avatar}
-            alt="Profile Photo"
+            className="w-[30rem] rounded-t-3xl"
+            src={bgCard}
+            alt="Background Card"
           />
+          <div className="w-[100%] pb-24 flex justify-center items-center">
+            <Image
+              className="absolute w-36 rounded-full border-white border-8"
+              src={avatar}
+              alt="Profile Photo"
+            />
+          </div>
+
+          <article className="w-[100%] flex flex-col justify-center items-center text-center">
+            <section className="w-max flex flex-row">
+              <h1 className="font-bold text-2xl text-very-dark">
+                Victor Crest
+              </h1>
+              <h2 className="pl-3 text-2xl text-dark-grayish">26</h2>
+            </section>
+            <section>
+              <h2 className="pt-2 font-normal text-lg text-dark-gray">
+                London
+              </h2>
+            </section>
+
+            <div className="w-[100%] pt-6 border-b-2 border-dark-gray opacity-20" />
+
+            <section className="w-[100%] pt-8 flex flex-row gap-20 justify-center items-center">
+              <article>
+                <h2 className="font-bold text-2xl text-very-dark">80K</h2>
+                <h3 className="font-normal tracking-widest text-sm text-dark-gray">
+                  Followers
+                </h3>
+              </article>
+              <article>
+                <h2 className="font-bold text-2xl text-very-dark">803K</h2>
+                <h3 className="font-normal tracking-widest text-sm text-dark-gray">
+                  Likes
+                </h3>
+              </article>
+              <article>
+                <h2 className="font-bold text-2xl text-very-dark">1.4K</h2>
+                <h3 className="font-normal tracking-widest text-sm text-dark-gray">
+                  Photos
+                </h3>
+              </article>
+            </section>
+          </article>
         </section>
       </section>
     </main>
